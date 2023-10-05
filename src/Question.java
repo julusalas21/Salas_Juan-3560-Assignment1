d4f74316052a501b1a4ae52b39d6b881ba02e961
julusalas21 <118496249+julusalas21@users.noreply.github.com>
2023-10-05 02:13:19 -0700

    beta completed question class

--- src/Question.java
+++ src/Question.java
@@ -1,7 +1,23 @@
+import java.util.Vector;
+
 public abstract class Question {
+    //the main parts of a question class are the question itself, the number of answers,
+    //and the answer choices that are listed
     private String question;
     private int numOfAnswers;
+    private Vector<String> AnswerChoices= new Vector<String>();
 
+    public void setAnswerChoices(Vector<String> answerChoices){
+        AnswerChoices=(Vector)answerChoices.clone();
+    };
+
+    public String getAnswerChoice(int index){
+        String answerChoice = AnswerChoices.elementAt(index);
+        return answerChoice;
+    };
+
+    // the correct answer is the next important part of a question
+    //however, that needs to be handled by the children which go into more detail
     public abstract void AnswerKey();
 
     public void setNumOfAnswers(int numOfAnswers) {
@@ -15,6 +31,7 @@ public abstract class Question {
     public void setQuestion(String usrQuestion) {
         question=usrQuestion;
     }
+
     public String getQuestion() {
         return question;
     }
